70b62e95469601b964ecb6e8c0a2899f08d6bbc3
EssEnemiGz <essenemiggz@gmail.com>
2023-12-10 15:01:57 -0400

    "AHORA SI PAPA, ya podemos subir bases de datos en formato json y se cambia de una"

--- static/js/script.js
+++ static/js/script.js
@@ -1,68 +1,111 @@
 // Datos para la gráfica de barras
-const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
-const data = [12, 19, 3, 5, 2];
+const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+let data = [];
+for (var i=0;i<11;i++){
+    data.push(Math.floor(Math.random() * 1000) + 1);
+}
 
-// Crear la instancia de la gráfica
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: 'Ventas por mes',
-            data: data,
-            backgroundColor: '#00003D', // Color de las barras
-            borderColor: '#8C898A', // Borde de las barras
-            borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-            y: {
-                beginAtZero: true // Empezar desde cero en el eje Y
-            }
-        }
-}});
+let myChart = null;
+let myChart2 = null;
+let myChart3 = null;
 
-const ctx2 = document.getElementById('myChart2').getContext('2d');
-var barColors = ['#4682B4', '#87CEEB', '#6495ED', '#ADD8E6', '#1E90FF'];
-const myChart2 = new Chart(ctx2, {
-    type: 'pie',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: 'Ventas por mes',
-            data: data,
-            backgroundColor: barColors, // Color de las barras
-            borderColor: '#8C898A', // Borde de las barras
-            borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-            y: {
-                beginAtZero: true // Empezar desde cero en el eje Y
-            }
-        }
-}});
+function destruirGraficas() {
+    if (myChart) {
+        myChart.destroy();
+    }
+    if (myChart2) {
+        myChart2.destroy();
+    }
+    if (myChart3) {
+        myChart3.destroy();
+    }
+}
 
-const ctx3 = document.getElementById('myChart3').getContext('2d');
-const myChart3 = new Chart(ctx3, {
-    type: 'bar',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: 'Ventas por mes',
-            data: data,
-            backgroundColor: barColors, // Color de las barras
-            borderColor: '#8C898A', // Borde de las barras
-            borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-            y: {
-                beginAtZero: true // Empezar desde cero en el eje Y
+function generarGráfica(){
+    destruirGraficas();
+    // Crear la instancia de la gráfica
+    const ctx = document.getElementById('myChart').getContext('2d');
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Ventas por mes',
+                data: data,
+                backgroundColor: '#00003D', // Color de las barras
+                borderColor: '#8C898A', // Borde de las barras
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true // Empezar desde cero en el eje Y
+                }
             }
+    }});
+
+    const ctx2 = document.getElementById('myChart2').getContext('2d');
+    var barColors = ['#4682B4', '#87CEEB', '#6495ED', '#ADD8E6', '#1E90FF'];
+    myChart2 = new Chart(ctx2, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Ventas por mes',
+                data: data,
+                backgroundColor: barColors, // Color de las barras
+                borderColor: '#8C898A', // Borde de las barras
+                borderWidth: 1
+            }]
         }
-}});
+    });
+
+    const ctx3 = document.getElementById('myChart3').getContext('2d');
+    myChart3 = new Chart(ctx3, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Ventas por mes',
+                data: data,
+                backgroundColor: barColors, // Color de las barras
+                borderColor: '#8C898A', // Borde de las barras
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true // Empezar desde cero en el eje Y
+                }
+            }
+    }});
+}
+
+generarGráfica();
+
+function cargarJSON() {
+    const file = document.getElementById('up__input').files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function(event) {
+        try {
+          const objetoJSON = JSON.parse(event.target.result);
+          data = objetoJSON;
+          generarGráfica();
+
+          // Aquí puedes trabajar con el objeto JSON como desees
+          // Por ejemplo:
+          // console.log(objetoJSON);
+          // objetoJSON.propiedad...
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      reader.readAsText(file);
+    }
+}
