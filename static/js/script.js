82a74107f1cc2220119c27ce540cfea6335cc37f
EssEnemiGz <essenemiggz@gmail.com>
2023-12-10 13:22:24 -0400

    "New interface"

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,46 @@ const myChart = new Chart(ctx, {
             }
         }
 }});
+
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+var barColors = ['#4682B4', '#87CEEB', '#6495ED', '#ADD8E6', '#1E90FF'];
+const myChart2 = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Ventas por mes',
+            data: data,
+            backgroundColor: barColors, // Color de las barras
+            borderColor: '#8C898A', // Borde de las barras
+            borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+            y: {
+                beginAtZero: true // Empezar desde cero en el eje Y
+            }
+        }
+}});
+
+const ctx3 = document.getElementById('myChart3').getContext('2d');
+const myChart3 = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Ventas por mes',
+            data: data,
+            backgroundColor: barColors, // Color de las barras
+            borderColor: '#8C898A', // Borde de las barras
+            borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+            y: {
+                beginAtZero: true // Empezar desde cero en el eje Y
+            }
+        }
+}});
