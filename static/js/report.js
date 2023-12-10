1a6682dab30d92b45d09e84752e9eb9f89388a40
EssEnemiGz <essenemiggz@gmail.com>
2023-12-10 19:34:49 -0400

    "Versión final, generación de reportes terminada, esperando por más apartados para el reporte o por un footer"

--- static/js/report.js
+++ static/js/report.js
@@ -2,14 +2,10 @@
 const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 let data = [];
 
-let myChart = null;
 let myChart2 = null;
 let myChart3 = null;
 
 function destruirGraficas() {
-    if (myChart) {
-        myChart.destroy();
-    }
     if (myChart2) {
         myChart2.destroy();
     }
@@ -21,27 +17,6 @@ function destruirGraficas() {
 function generarGráfica(){
     destruirGraficas();
     // Crear la instancia de la gráfica
-    const ctx = document.getElementById('myChart').getContext('2d');
-    myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Ventas por mes',
-                data: data,
-                backgroundColor: '#00003D', // Color de las barras
-                borderColor: '#8C898A', // Borde de las barras
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true // Empezar desde cero en el eje Y
-                }
-            }
-    }});
-
     const ctx2 = document.getElementById('myChart2').getContext('2d');
     var barColors = ['#4682B4', '#87CEEB', '#6495ED', '#ADD8E6', '#1E90FF'];
     myChart2 = new Chart(ctx2, {
@@ -92,8 +67,27 @@ function cargarJSON() {
         try {
           const objetoJSON = JSON.parse(event.target.result);
           data = objetoJSON;
-          generarGráfica();
+          generarGráfica()
+          // TO WORK WITH THE JSON FILE
+          
+          let fechaActual = new Date();          
+          let año = fechaActual.getFullYear();
+          let mes = fechaActual.getMonth() + 1; // Los meses van de 0 a 11, por eso se suma 1
+          let dia = fechaActual.getDate();          
+          let fechaFormateada = `${año}-${mes < 10 ? '0' + mes : mes}-${dia < 10 ? '0' + dia : dia}`;
 
+          document.querySelector(".db-name").innerText = "El presente informe detalla un análisis de datos obtenidos de la base de datos "+file['name']+".";
+          document.querySelector(".date").innerText = fechaFormateada;
+          document.querySelector(".numero-registros").innerText = objetoJSON.length+1+" registros fueron encontrados por el sistema";
+          document.querySelector(".size").innerText = "El tamaño total estimado de la base de datos es de "+file['size']+"KB";
+          let tendencia;
+          if (objetoJSON[10] > objetoJSON[0]){
+            tendencia = "La tendencia actual es una subida, con una diferencia de "+String(objetoJSON[10]-objetoJSON[0])+" ventas con respecto al primer mes del año."
+          }
+          else{
+            tendencia = "La tendencia actual es una bajada, con una diferencia de "+String(objetoJSON[0]-objetoJSON[10])+" ventas con respecto al primer mes del año."
+          }
+          document.querySelector(".tendencia").innerText = tendencia;
         } catch (error) {
           console.error(error);
         }
